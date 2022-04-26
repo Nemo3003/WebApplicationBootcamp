18f1624a95ff07c32f03f109076515a235059b00
Nemo3003 <ulimarian2000@gmail.com>
2022-04-25 22:26:09 -0300

    'classes'

---
+++ Ejercicios3y4/Classes/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace Ejercicios3y4.Classes
+{
+    public static class Class
+    {
+        public static int GetNumber(int unNum)
+        {
+            return unNum;
+        }
+    }
+}
